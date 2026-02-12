1ad8bc5f14a5a2dd40d92e2d2b7954b741668fb3
sergey88889999 <sergey88889999@gmail.com>
2026-02-12 08:09:39 +0100

    upd: add gcc stage 2 build script

---
+++ stage00/gcc-s2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# File: stage00/gcc-s2.sh
+set -e
+
+echo "---------------- gcc-s2.sh -----------------------"
+echo "--- Шаг 0: Настройка окружения ---"
+CORES=$(nproc)
+export MAKEFLAGS="-j$CORES"
+export WORK_DIR=$HOME/work
+export PREFIX=$HOME/toolchain
+export TARGET=x86_64-custom-linux-gnu
+export PATH="$PREFIX/bin:$PATH"
+
+VERSION_GCC="15.2.0"
+
+mkdir -p $WORK_DIR
+cd $WORK_DIR
+
+echo "--- Шаг 1: Подготовка исходников ---"
+# Мы скачиваем заново или используем кэш, чтобы папка была чистой
+wget -c https://ftp.gnu.org/gnu/gcc/gcc-${VERSION_GCC}/gcc-${VERSION_GCC}.tar.xz
+tar -xJf gcc-${VERSION_GCC}.tar.xz
+cd gcc-${VERSION_GCC}
+./contrib/download_prerequisites
+cd ..
+
+echo "--- Шаг 2: Конфигурация Stage 2 (Финальный кросс-компилятор) ---"
+rm -rf gcc-build-final && mkdir gcc-build-final && cd gcc-build-final
+
+# ГЛАВНЫЕ ИЗМЕНЕНИЯ ЗДЕСЬ:
+# 1. Убрали --with-newlib и --without-headers
+# 2. Включили поддержку потоков posix
+# 3. Включили сборку shared-библиотек (libstdc++.so)
+../gcc-${VERSION_GCC}/configure --target=$TARGET \
+    --prefix=$PREFIX \
+    --with-sysroot=$PREFIX/$TARGET \
+    --enable-languages=c,c++ \
+    --enable-shared \
+    --enable-threads=posix \
+    --enable-libstdcxx \
+    --disable-multilib \
+    --disable-nls \
+    --disable-bootstrap \
+    --with-system-zlib
+
+echo "--- Шаг 3: Компиляция ---"
+make 
+make install
+
+echo "--- Шаг 4: Проверка ---"
+echo 'int main(){return 0;}' > test.c
+$PREFIX/bin/$TARGET-gcc test.c -o test
+file test
+
+echo "--- Шаг 5: Очистка ---"
+cd $WORK_DIR
+rm -rf gcc-${VERSION_GCC} gcc-build-final
+
+echo "---------------- gcc-s2.sh DONE ------------------"
